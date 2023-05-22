ba087d42d273c88e392757a67e4b835a83cae441
Nevyn Bengtsson <nevyn@alloverse.com>
2023-05-22 14:08:41 +0200

    falling thingies

---
+++ Assets/ThingieSpawner.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingieSpawner : MonoBehaviour
+{
+    public GameObject[] things;
+    public float range = 50.0f;
+    public int amount = 2000;
+
+
+    void Start()
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            GameObject prefab = things[Random.Range(0, things.Length)];
+            Instantiate(
+                prefab, 
+                transform.position + new Vector3(
+                    Random.Range(-range, range),
+                    Random.Range(-range, range)*0.1f,
+                    Random.Range(-range, range)
+                ), 
+                Quaternion.identity
+            );
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void Startf()
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            GameObject thingie = things[Random.Range(0, things.Length)];
+            Instantiate(
+                thingie, 
+                transform.position + new Vector3(
+                    Random.Range(-range, range),
+                    Random.Range(-range*0.1f, range*0.1f),
+                    Random.Range(-range, range)
+                ), 
+                Random.rotation
+            );
+        }
+    }
+}
