4a0f0e496a2569bd9c92e00db294e857cc719824
Nevyn Bengtsson <nevyn@alloverse.com>
2023-05-22 16:23:22 +0200

    fix

--- Assets/ThingieSpawner.cs
+++ Assets/ThingieSpawner.cs
@@ -21,35 +21,6 @@ public class ThingieSpawner : MonoBehaviour
                     Random.Range(-range, range)*0.1f,
                     Random.Range(-range, range)
                 ), 
-                Quaternion.identity
-            );
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    void Startf()
-    {
-        for(int i = 0; i < amount; i++)
-        {
-            GameObject thingie = things[Random.Range(0, things.Length)];
-            Instantiate(
-                thingie, 
-                transform.position + new Vector3(
-                    Random.Range(-range, range),
-                    Random.Range(-range*0.1f, range*0.1f),
-                    Random.Range(-range, range)
-                ), 
                 Random.rotation
             );
         }
